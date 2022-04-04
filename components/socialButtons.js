8a8c8ea998c8c2f56d96b61aa416b638adb4ca93
Muddasir E <muddasir.e@bigbinary.com>
2022-04-04 14:37:05 +0530

    social fix

--- components/socialButtons.js
+++ components/socialButtons.js
@@ -1,5 +1,11 @@
+import { useEffect, useState } from "react";
+
 const SocialButtons = ({ title }) => {
-  const pageLink = "window.location.href";
+  const [pageLink, setPageLink] = useState(null);
+  useEffect(() => {
+    setPageLink(window.location.href);
+  }, []);
+
   const facebookShare = () => {
     window.open(
       `https://facebook.com/share.php?u=${pageLink}&amp;t=${title}`,
@@ -17,31 +23,33 @@ const SocialButtons = ({ title }) => {
 
   return (
     <>
-      <div className="footer-social-icons flex items-end text-right mr-5">
-        <ul className="social-icons mr-5">
-          <li onClick={facebookShare}>
-            <a href="" className="social-icon">
-              {" "}
-              <i className="fa fa-facebook"></i>
-            </a>
-          </li>
-          <li onClick={twitterShare}>
-            <a href="" className="social-icon">
-              {" "}
-              <i className="fa fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href={`whatsapp://send?text=${title} - ${pageLink}`}
-              className="social-icon"
-            >
-              {" "}
-              <span href="" className="fa fa-whatsapp"></span>
-            </a>
-          </li>
-        </ul>
-      </div>
+      {pageLink && (
+        <div className="footer-social-icons flex items-end text-right mr-5">
+          <ul className="social-icons mr-5">
+            <li onClick={facebookShare}>
+              <a href="" className="social-icon">
+                {" "}
+                <i className="fa fa-facebook"></i>
+              </a>
+            </li>
+            <li onClick={twitterShare}>
+              <a href="" className="social-icon">
+                {" "}
+                <i className="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href={`whatsapp://send?text=${title} - ${pageLink}`}
+                className="social-icon"
+              >
+                {" "}
+                <span href="" className="fa fa-whatsapp"></span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </>
   );
 };
