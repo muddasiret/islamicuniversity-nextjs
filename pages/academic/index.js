98cc95817202e971be4dbd4feb6c54ea090ab5ec
Muddasir E <muddasir.e@bigbinary.com>
2022-08-09 15:29:06 +0530

    semi and academic

--- pages/academic/index.js
+++ pages/academic/index.js
@@ -5,21 +5,29 @@ import ProgrammeCard from "../../components/programmeCard";
 import { fetchAPI } from "../../lib/api";
 import { getTitleImage } from "../../utils/getTitleImage";
 
-const Academic = ({ programmes,global }) => {
-  let title_image =  getTitleImage(global,"Academic")
+const Academic = ({ programmes, global }) => {
+  let title_image = getTitleImage(global, "Academic");
 
   return (
     <Layout>
-      <PageTitle title="Academic" title_image={title_image}/>
+      <PageTitle title="Academic" title_image={title_image} />
       <MainLayout>
-        <h1 className="my-5 md:text-5xl text-4xl sanspro font-black uppercase text-primaryblue text-center">Study</h1>
+        <h1 className="my-5 md:text-5xl text-4xl sanspro font-black uppercase text-primaryblue text-center">
+          Study
+        </h1>
         <p className="md:my-2 sanspro md:text-2xl text-xl font-semibold text-black-700 text-center">
           We currently offer three full and part time academic programmes.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {programmes.map((item, ind) => {
-            return <ProgrammeCard details={item} key={ind} />;
-          })}
+          {programmes.length ? (
+            programmes.map((item, ind) => {
+              return <ProgrammeCard details={item} key={ind} />;
+            })
+          ) : (
+            <div className="my-20 text-3xl text-center w-full flex justify-center">
+              No Data for Academic
+            </div>
+          )}
         </div>
       </MainLayout>
     </Layout>
@@ -29,7 +37,12 @@ const Academic = ({ programmes,global }) => {
 export async function getStaticProps() {
   // Run API calls in parallel
   const [programmes] = await Promise.all([
-    fetchAPI("/programmes", { populate: "image" }),
+    fetchAPI("/programmes", {
+      filters: {
+        type: "academic",
+      },
+      populate: "image",
+    }),
   ]);
 
   return {
--- pages/semi-academic/index.js
+++ pages/semi-academic/index.js
@@ -5,21 +5,27 @@ import ProgrammeCard from "../../components/programmeCard";
 import { fetchAPI } from "../../lib/api";
 import { getTitleImage } from "../../utils/getTitleImage";
 
-const SemiAcademic = ({ programmes,global }) => {
-  let title_image =  getTitleImage(global,"Semi Academic")
+const SemiAcademic = ({ programmes, global }) => {
+  let title_image = getTitleImage(global, "Semi Academic");
 
   return (
     <Layout>
-      <PageTitle title="Semi Academic" title_image={title_image}/>
+      <PageTitle title="Semi Academic" title_image={title_image} />
       <MainLayout>
-        <h1 className="my-5 md:text-5xl text-4xl sanspro font-black uppercase text-primaryblue text-center">Study</h1>
+        <h1 className="my-5 md:text-5xl text-4xl sanspro font-black uppercase text-primaryblue text-center">
+          Study
+        </h1>
         <p className="md:my-2 sanspro md:text-2xl text-xl font-semibold text-black-700 text-center">
           We currently offer three full and part time academic programmes.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {programmes.map((item, ind) => {
-            return <ProgrammeCard details={item} key={ind} />;
-          })}
+          {programmes.length ? (
+            programmes.map((item, ind) => {
+              return <ProgrammeCard details={item} key={ind} />;
+            })
+          ) : (
+            <div className="my-20 text-3xl text-center w-full flex justify-center">No Data for Semi Academic</div>
+          )}
         </div>
       </MainLayout>
     </Layout>
@@ -29,7 +35,12 @@ const SemiAcademic = ({ programmes,global }) => {
 export async function getStaticProps() {
   // Run API calls in parallel
   const [programmes] = await Promise.all([
-    fetchAPI("/programmes", { populate: "image" }),
+    fetchAPI("/programmes", {
+      filters: {
+        type: "semi_academic",
+      },
+      populate: "image",
+    }),
   ]);
 
   return {
